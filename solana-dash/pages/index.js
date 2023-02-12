a18f8ee7103330652c878db9c1fc6e5442ab2e80
unknown <thongqtran@outlook.com>
2023-02-12 22:27:39 +0700

    add project scalffolding

---
+++ solana-dash/pages/index.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+     <h1 className={styles.code}>Solana Dash</h1>
+     <p className={styles.description}>Dashboard for all solana wallet address</p>
+    </>
+  )
+}
