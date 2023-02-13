0c7fe0b4df26c5872d3eb330200704285ce40191
unknown <thongqtran@outlook.com>
2023-02-14 03:28:24 +0700

    add more stylings for header

--- pages/layout/main_header.js
+++ pages/layout/main_header.js
@@ -3,16 +3,22 @@ import styles from './main_header.module.css'
 
 function MainHeader() {
     return(
-        <div className="flex space-x-12 bg-gray-900 shadow-sm p-5">
+        <div className="flex space-x-12 bg-zinc-900 shadow-2xl p-5">
             <div>
-                <img className={styles.logo} src="solana-dash-logo.png" alt="page-logo"/>
+                <Link href="/"><img className={styles.logo} src="solana-dash-logo.png" alt="page-logo"/></Link>
             </div>
             <nav>
                 <ul className="flex space-x-6">
-                    <li className="uppercase"><Link href="/about">About</Link></li>
-                    <li className="uppercase"><Link href="/">Contact</Link></li>
+                    <li className="uppercase p-2 bg-zinc-800 rounded-md"><Link href="/about">About</Link></li>
+                    <li className="uppercase p-2 bg-zinc-800 rounded-md"><Link href="/contact">Contact</Link></li>
                 </ul>
             </nav>
+
+            <div className="space-x-2">
+                <input className="p-3 bg-zinc-800 rounded-md w-auto" type="text" placeholder='enter your wallet address...'></input>
+                <button className="p-3 bg-zinc-800 rounded-md" >Check Wallet</button>
+            </div>
+            
         </div>
     )
 }
