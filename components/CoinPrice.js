8dd90da4485b9dfd4ff54ca51ec7ad04ac921ad8
unknown <thongqtran@outlook.com>
2023-02-18 01:40:20 +0700

    add meta data, optimize image

--- components/CoinPrice.js
+++ components/CoinPrice.js
@@ -1,11 +1,14 @@
+import Image from 'next/image'
+
 function CoinPrice(props) {
     const {coinTicker, coinLogo, price} = props
 
     return(
         <div className="flex flex-row space-x-1">
-            <img className="rounded-full w-9 h-9 mt-1.5" src={`./${coinLogo}`} alt="sol coin logo" />
-            <h1 className="bg-zinc-600 rounded-lg p-1">{coinTicker}</h1>
-            <h1 className="bg-blue-700 rounded-lg p-1">${price.toFixed(3)} USD</h1>
+            <Image className="rounded-full w-9 h-9 mt-1.5" src={`/${coinLogo}`} alt="sol coin logo" width={60} height={60}/>
+            {/* <img className="rounded-full w-9 h-9 mt-1.5" src={`./${coinLogo}`} alt="sol coin logo" /> */}
+            <h1 className="bg-zinc-600 rounded-lg p-2">{coinTicker}</h1>
+            <h1 className="bg-blue-700 rounded-lg p-2">${price.toFixed(2)} USD</h1>
         </div>
     )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
+import Head from 'next/head'
+
 import '@/styles/globals.css'
 import Layout from './layout/layout'
 
 export default function App({ Component, pageProps }) {
   return <Layout className="bg-black">
+    <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <title>Solana Dash_</title>
+        <link rel="icon" type="image/x-icon" href="solana-dash-logo.png"></link>
+    </Head>
     <Component {...pageProps} />
     </Layout>
 }
